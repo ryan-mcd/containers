fc33a3c609fe094a64b4416a5c2ed56ccfa08e03
Ryan-McD <72583999+Ryan-McD@users.noreply.github.com>
2023-11-03 21:50:12 -0400

    Revert "chore: add kopia 0.14.1"
    This reverts commit 4f064ff2aa6cb8696bdf60e4c27722be75d8631e.

--- apps/kopia/ci/latest.sh
+++ apps/kopia/ci/latest.sh
@@ -1,8 +1,5 @@
-# #!/usr/bin/env bash
-# version=$(curl -sX GET "https://api.github.com/repos/kopia/kopia/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-# version="${version#*v}"
-# version="${version#*release-}"
-
-# # Need version 0.14.x
-version="0.14.1"
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://api.github.com/repos/kopia/kopia/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
