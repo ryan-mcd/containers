4f064ff2aa6cb8696bdf60e4c27722be75d8631e
Ryan-McD <72583999+Ryan-McD@users.noreply.github.com>
2023-11-03 21:40:23 -0400

    chore: add kopia 0.14.1

--- apps/kopia/ci/latest.sh
+++ apps/kopia/ci/latest.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/kopia/kopia/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
+# #!/usr/bin/env bash
+# version=$(curl -sX GET "https://api.github.com/repos/kopia/kopia/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+# version="${version#*v}"
+# version="${version#*release-}"
+
+# # Need version 0.14.x
+version="0.14.1"
 printf "%s" "${version}"
